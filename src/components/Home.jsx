df666bb5424d8750d0dbb945865805e0fe812766
MylesMburu <ngicumyles@gmail.com>
2023-02-23 22:05:23 +0300

    Created the raw template

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+        <a href="#">Log in</a>
+        <a href="#">Sign up</a>
+          <header>
+            <h2>Dishi</h2>
+            <h3>Recipies</h3>
+          </header>
+        </div>
+
+       <div>
+        <h4>Latest Recipies</h4>
+        <div>
+            {/* cards */}
+            <img src="" alt="" />
+            <div>
+                <span>Githeri noma ya njahi</span>
+            </div>
+        </div>
+       </div>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+        <div>
+            <h1><a href="/">Dishi</a></h1>
+        </div>
+        <ul>
+            <li>
+                <a href="#">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>About</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>Contacts</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
