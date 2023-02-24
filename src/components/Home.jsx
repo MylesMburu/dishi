91c28c943fa7ce7f4fcde8dbacfdcaf42f223829
MylesMburu <ngicumyles@gmail.com>
2023-02-24 21:39:18 +0300

    Text styling using tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import IMG1 from '../assets/githeri.jpg'
 
 const Home = () => {
   return (
-    <div>
+    <div className='text-red-700'>
         <div>
         <a href="#">Log in</a>
         <a href="#">Sign up</a>
           <header>
-            <h2>Dishi</h2>
-            <h3>Recipies</h3>
+            <h2 className='text-gray-700 text-3xl uppercase'>Dishi</h2>
+            <h3 className='text-gray-700 text-2xl'>Recipies</h3>
           </header>
         </div>
 
        <div>
-        <h4>Latest Recipies</h4>
+        <h4 className='font-bold'>Latest Recipies</h4>
         <div>
             {/* cards */}
-            <img src="" alt="" />
+            <img src={IMG1} alt="Image of githeri" />
             <div>
                 <span>Githeri noma ya njahi</span>
             </div>
