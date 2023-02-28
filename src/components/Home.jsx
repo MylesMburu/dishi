c47ad62c7bde49c3e9ccb7f07bbfaf95238d8040
MylesMburu <ngicumyles@gmail.com>
2023-02-28 21:21:04 +0300

    flex box in tw

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,11 @@ const Home = () => {
   return (
     <div className='text-red-700 px-16 py-6 font-Poppins'>
         <div>
-        <a href="#">Log in</a>
-        <a href="#">Sign up</a>
+          <div className="login flex justify-center gap-6 mb-9 md:justify-end ">
+          <a href="#">Log in</a>
+          <a href="#">Sign up</a>
+          </div>
+        
           <header>
             <h2 className='text-gray-700 text-3xl uppercase'>Dishi</h2>
             <h3 className='text-gray-700 text-2xl'>Recipies</h3>
@@ -15,7 +18,7 @@ const Home = () => {
 
        <div>
         <h4 className='text-brown-700 font-bold mt-6 pb-2 border-b border-gray-300'>Latest Recipies</h4>
-        <div className='mt-8'>
+        <div className='card mt-8 rounded overflow-hidden'>
             {/* cards */}
             <img src={IMG1} alt="Image of githeri" />
             <div>
