82f5bf13847a6149fe72599eef2341e1a1c764f7
MylesMburu <ngicumyles@gmail.com>
2023-03-19 17:17:38 +0300

    ..

--- sever/server.js
+++ sever/server.js
@@ -1,9 +1,19 @@
-app.listen
+const express = require('express');
+const app = express();
 
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+app.get('/meals/:query', (req, res) => {
+  const searchQuery = req.params.query; // get the search query from the request params
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`;
 
-fetch(url + "pizza")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-    });
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // this will log the data to the console
+      res.send(data); // send the data back to the client as a response
+    })
+    .catch(error => console.error(error));
+});
+
+app.listen(3001, () => {
+  console.log('Server is listening on port 3001');
+});
